bf6373508efad0014d56d44c157d2a6a67b8aca7
Yusuf Alp <alpyusuf007@gmail.com>
2020-10-16 12:49:24 -0700

    Updated App test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
