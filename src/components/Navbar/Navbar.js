73f685a297534223b6c2d7f8731e4b480c9b66ed
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-30 10:46:43 -0800

    implement a logout button for authorized user

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './Navbar.css';
 import { Link, NavLink } from 'react-router-dom';
+import TokenService from '../../services/token-service';
 
 class Navbar extends React.Component {
   state = {
     menuVisible: true
   }
+
+  handleLogoutClick = () => {
+  }
+
   handleMenu = () => {
     this.setState({
       menuVisible: !this.state.menuVisible
     })
+  }
+
+  renderLogoutLink() {
+    return (
+      <div className={`navbar-links ${this.state.menuVisible ? 'open-menu' : ''}`}>
+        <Link
+          onClick={this.handleLogoutClick}
+          to='/'>
+          Logout
+        </Link>
+      </div>
+    )
+  }
+
+  renderLoginLink() {
+    return (
+      <div
+        className={`navbar-links ${this.state.menuVisible ? 'open-menu' : ''}`}>
+        <Link to='/'>Home</Link>
+        <NavLink to='/demo'>Demo</NavLink>
+        <NavLink to='/pricing'>Pricing</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/contact'>Contact</NavLink>
+      </div>
+    )
   }
   render() {
     return (
@@ -20,14 +50,9 @@ class Navbar extends React.Component {
           onClick={this.handleMenu}
         ><i className='fas fa-chevron-circle-down'></i>
         </button>
-        <div
-          className={`navbar-links ${this.state.menuVisible ? 'open-menu' : ''}`}>
-          <Link to='/'>Home</Link>
-          <NavLink to='/demo'>Demo</NavLink>
-          <NavLink to='/pricing'>Pricing</NavLink>
-          <NavLink to='/about'>About</NavLink>
-          <NavLink to='/contact'>Contact</NavLink>
-        </div>
+        {TokenService.hasAuthToken()
+          ? this.renderLogoutLink()
+          : this.renderLoginLink()}
       </nav>
     )
   }
