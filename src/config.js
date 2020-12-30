bbf67bd809bfe867054cb2256cbe0f86aba32ac4
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-30 09:13:34 -0800

    Add a token key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
-  BASE_API_URL: 'https://shielded-meadow-94057.herokuapp.com'
+  BASE_API_URL: 'https://shielded-meadow-94057.herokuapp.com',
   // BASE_API_URL: 'http://localhost:8000'
+  TOKEN_KEY: 'gleamsis-client-auth-token',
 }
 
 export default config;
