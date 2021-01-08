187c65c316e88fcfff92f85e538c3f4f010c303b
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-08 08:21:30 -0800

    Update the expenditures table to display information in a better organized fashion

---
+++ src/components/Expenditures/Expenditure.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+});
+
+const Expenditure = (props) => {
+  return (
+    <div className='expenditure'>
+      <p className='description'>{props.expenditure.description}</p>
+      <p className='budget'>{formatter.format(props.expenditure.budget)}</p>
+      <p className='actual'>{formatter.format(props.expenditure.actual)}</p>
+    </div>
+  );
+}
+
+export default Expenditure;
---
+++ src/components/Expenditures/Expenditures.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Expenditure from './Expenditure';
+import './Expenditure.css';
+
+const expenditures = [
+  {
+    id: 0,
+    description: 'Teacher Salary',
+    budget: 86000,
+    actual: 87432,
+  },
+  {
+    id: 1,
+    description: 'Books',
+    budget: 23135,
+    actual: 23246,
+  },
+  {
+    id: 2,
+    description: 'Sanitation',
+    budget: 2500,
+    actual: 4500,
+  },
+  {
+    id: 3,
+    description: 'Lunch',
+    budget: 3453,
+    actual: 3453,
+  },
+];
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+});
+
+export default function ExpenditureTable() {
+  const renderedExpenditure = expenditures.map(expenditure => (
+    <Expenditure key={expenditure.id} expenditure={expenditure} />
+  ));
+
+  const totalBudget = expenditures.reduce((acc, cur) => {
+    return acc + cur.budget
+  }, 0);
+
+  const totalActual = expenditures.reduce((acc, cur) => {
+    return acc + cur.actual
+  }, 0);
+
+  return (
+    <div>
+      <h3>Expenditure</h3>
+      <div className='expenditure'>
+        <p className='title description'>Description</p>
+        <p className='title budget'>Budget</p>
+        <p className='title actual'>Actual</p>
+      </div>
+      {renderedExpenditure}
+      <div className='expenditure'>
+        <p className='title description'>TOTAL</p>
+        <p className='title budget'>{formatter.format(totalBudget)}</p>
+        <p className='title actual'>{formatter.format(totalActual)}</p>
+      </div>
+    </div>
+  );
+}
