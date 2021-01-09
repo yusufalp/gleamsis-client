8fa9e6eec96e436e2b2261984c4ed3463fc850c1
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-09 06:59:21 -0800

    Convert it to function component

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-class Page404 extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Page not found</h1>
-      </div>
-    );
-  }
+const Page404 = () => {
+  return (
+    <div>
+      <h1>Page not found</h1>
+    </div>
+  );
+
 }
 
 export default Page404;
